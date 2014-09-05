7548feba11e580c03166a85731eb695717039dda
weizili.build9 <zieckey@gmail.com>
2014-09-05 18:34:27 +0800

    rename nginx command name

--- ext-module/http_get_filter/ngx_http_get_filter_module.c
+++ ext-module/http_get_filter/ngx_http_get_filter_module.c
@@ -6,26 +6,26 @@
 typedef struct
 {
     ngx_str_t		stock[6];
-} ngx_http_subrequest_test_ctx_t;
+} ngx_http_http_get_filter_ctx_t;
 
 
 static char *
-ngx_http_subrequest_test(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+ngx_http_http_get_filter(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 
-static ngx_int_t ngx_http_subrequest_test_handler(ngx_http_request_t *r);
-static ngx_int_t subrequest_test_subrequest_post_handler(ngx_http_request_t *r, void *data, ngx_int_t rc);
-static void subrequest_test_post_handler(ngx_http_request_t * r);
-
-
+static ngx_int_t ngx_http_http_get_filter_handler(ngx_http_request_t *r);
+static ngx_int_t http_get_filter_subrequest_post_handler(ngx_http_request_t *r, void *data, ngx_int_t rc);
+static void http_get_filter_post_handler(ngx_http_request_t * r);
 
 
-static ngx_command_t  ngx_http_subrequest_test_commands[] =
+
+
+static ngx_command_t  ngx_http_http_get_filter_commands[] =
 {
 
     {
-        ngx_string("subrequest_test"),
+        ngx_string("http_get_filter"),
             NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-            ngx_http_subrequest_test,
+            ngx_http_http_get_filter,
             NGX_HTTP_LOC_CONF_OFFSET,
             0,
             NULL
@@ -53,7 +53,7 @@ ngx_module_t  ngx_http_get_filter_module =
 {
     NGX_MODULE_V1,
     &ngx_http_get_filter_module_ctx,           /* module context */
-    ngx_http_subrequest_test_commands,              /* module directives */
+    ngx_http_http_get_filter_commands,              /* module directives */
     NGX_HTTP_MODULE,                       /* module type */
     NULL,                                  /* init master */
     NULL,                                  /* init module */
@@ -65,7 +65,7 @@ ngx_module_t  ngx_http_get_filter_module =
     NGX_MODULE_V1_PADDING
 };
 
-static ngx_int_t subrequest_test_subrequest_post_handler(ngx_http_request_t *r,
+static ngx_int_t http_get_filter_subrequest_post_handler(ngx_http_request_t *r,
                                                          void *data, ngx_int_t rc)
 {
     ngx_buf_t* pRecvBuf;
@@ -78,7 +78,7 @@ static ngx_int_t subrequest_test_subrequest_post_handler(ngx_http_request_t *r,
     //注意，上下文是保存在父请求中的（参见5.6.5节），所以要由pr中取上下文。
     //其实有更简单的方法，即参数data就是上下文，初始化subrequest时
     //我们就对其进行设置了的，这里仅为了说明如何获取到父请求的上下文
-    ngx_http_subrequest_test_ctx_t* myctx;
+    ngx_http_http_get_filter_ctx_t* myctx;
 
     pr = r->parent;
 
@@ -118,21 +118,21 @@ static ngx_int_t subrequest_test_subrequest_post_handler(ngx_http_request_t *r,
 
 
     //这一步很重要，设置接下来父请求的回调方法
-    pr->write_event_handler = subrequest_test_post_handler;
+    pr->write_event_handler = http_get_filter_post_handler;
 
     return NGX_OK;
 }
 
 
 static void
-subrequest_test_post_handler(ngx_http_request_t * r)
+http_get_filter_post_handler(ngx_http_request_t * r)
 {
     ngx_chain_t out;
     int bodylen;
     ngx_buf_t* b;
     ngx_int_t ret;
     ngx_str_t output_format;
-    ngx_http_subrequest_test_ctx_t* myctx;
+    ngx_http_http_get_filter_ctx_t* myctx;
     static ngx_str_t type = ngx_string("text/plain; charset=GBK");
 
     myctx = ngx_http_get_module_ctx(r, ngx_http_get_filter_module);
@@ -181,25 +181,25 @@ subrequest_test_post_handler(ngx_http_request_t * r)
 
 
 static char *
-ngx_http_subrequest_test(ngx_conf_t * cf, ngx_command_t * cmd, void * conf)
+ngx_http_http_get_filter(ngx_conf_t * cf, ngx_command_t * cmd, void * conf)
 {
     ngx_http_core_loc_conf_t  *clcf;
 
-    //首先找到subrequest_test配置项所属的配置块，clcf貌似是location块内的数据
+    //首先找到http_get_filter配置项所属的配置块，clcf貌似是location块内的数据
     //结构，其实不然，它可以是main、srv或者loc级别配置项，也就是说在每个
     //http{}和server{}内也都有一个ngx_http_core_loc_conf_t结构体
     clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
 
     //http框架在处理用户请求进行到NGX_HTTP_CONTENT_PHASE阶段时，如果
-    //请求的主机域名、URI与subrequest_test配置项所在的配置块相匹配，就将调用我们
-    //实现的ngx_http_subrequest_test_handler方法处理这个请求
-    clcf->handler = ngx_http_subrequest_test_handler;
+    //请求的主机域名、URI与http_get_filter配置项所在的配置块相匹配，就将调用我们
+    //实现的ngx_http_http_get_filter_handler方法处理这个请求
+    clcf->handler = ngx_http_http_get_filter_handler;
 
     return NGX_CONF_OK;
 }
 
 static ngx_int_t
-ngx_http_subrequest_test_handler(ngx_http_request_t * r)
+ngx_http_http_get_filter_handler(ngx_http_request_t * r)
 {
     ngx_http_post_subrequest_t *psr;
     ngx_str_t sub_prefix = ngx_string("/list=");
@@ -208,10 +208,10 @@ ngx_http_subrequest_test_handler(ngx_http_request_t * r)
     ngx_int_t rc;
 
     //创建http上下文
-    ngx_http_subrequest_test_ctx_t* myctx = ngx_http_get_module_ctx(r, ngx_http_get_filter_module);
+    ngx_http_http_get_filter_ctx_t* myctx = ngx_http_get_module_ctx(r, ngx_http_get_filter_module);
     if (myctx == NULL)
     {
-        myctx = ngx_palloc(r->pool, sizeof(ngx_http_subrequest_test_ctx_t));
+        myctx = ngx_palloc(r->pool, sizeof(ngx_http_http_get_filter_ctx_t));
         if (myctx == NULL)
         {
             return NGX_ERROR;
@@ -228,10 +228,10 @@ ngx_http_subrequest_test_handler(ngx_http_request_t * r)
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }
 
-    //设置子请求回调方法为subrequest_test_subrequest_post_handler
-    psr->handler = subrequest_test_subrequest_post_handler;
+    //设置子请求回调方法为http_get_filter_subrequest_post_handler
+    psr->handler = http_get_filter_subrequest_post_handler;
 
-    //data设为myctx上下文，这样回调subrequest_test_subrequest_post_handler
+    //data设为myctx上下文，这样回调http_get_filter_subrequest_post_handler
     //时传入的data参数就是myctx
     psr->data = myctx;
 
