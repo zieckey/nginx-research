e6b07c87d79119e75605304e14eb859c0c90d23f
weizili.build17 <zieckey@gmail.com>
2014-12-31 19:40:06 +0800

    add nginxlib basic types string  test

---
+++ nginxlib/build.mk
@@ -0,0 +1,27 @@
+
+CC=gcc
+CXX=g++
+AR=ar
+ARFLAGS=cru
+SRCS := $(wildcard *.c)
+OBJS := $(patsubst %.c, %.o, $(SRCS))
+
+NGINX_OBJS_PATH =../../nginx-1.0.15/objs
+NGINX_SRC_PATH  =../../nginx-1.0.15/src
+CFLAGS = -pipe -O -W -Wall -Wpointer-arith \
+		 -Wunused-value -Wno-unused-parameter -Wunused-function -Wunused-variable \
+		 -fPIC\
+		 -I $(NGINX_OBJS_PATH) \
+		 -I $(NGINX_SRC_PATH)/core \
+		 -I $(NGINX_SRC_PATH)/os \
+		 -I $(NGINX_SRC_PATH)/os/unix \
+		 -I $(NGINX_SRC_PATH)/os/event \
+
+LDFLAGS = -lpcre -lcrypto -lz
+
+TARGET=$(shell basename `pwd`)
+
+%.o : %.c
+	$(CC) -c $(CFLAGS) $< -o $@
+
+
