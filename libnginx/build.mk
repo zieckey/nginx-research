48f10158d99e93df7a0efe44ed16ac0ca1480680
weizili.build17 <zieckey@gmail.com>
2014-12-31 22:41:21 +0800

    delete nginx-1.7.9.tar.gz and download it instead

--- libnginx/build.mk
+++ libnginx/build.mk
@@ -7,8 +7,8 @@ SRCS := $(wildcard *.c)
 OBJS := $(patsubst %.c, %.o, $(SRCS))
 
 NGINX_VERSION = 1.7.9
-NGINX_OBJS_PATH = ../../nginx-$(NGINX_VERSION)/objs
-NGINX_SRC_PATH  = ../../nginx-$(NGINX_VERSION)/src
+NGINX_OBJS_PATH = ../nginx-$(NGINX_VERSION)/objs
+NGINX_SRC_PATH  = ../nginx-$(NGINX_VERSION)/src
 CFLAGS = -pipe -O -W -Wall -Wpointer-arith \
 		 -Wunused-value -Wno-unused-parameter -Wunused-function -Wunused-variable \
 		 -fPIC\
