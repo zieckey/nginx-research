877e603d6b28872a7f5c3890c94d66bafc95103c
weizili.360.laptop <zieckey@gmail.com>
2015-01-03 21:06:41 +0800

    add ngx_hash research code

--- libnginx/research_ngx_hash.cc
+++ libnginx/research_ngx_hash.cc
@@ -2,75 +2,68 @@
 #include "allinc.h"
 #include "dump_nginx_struct.h"
 
-// 
-// static ngx_str_t names[] = { 
-//         ngx_string("zieckey"),
-//         ngx_string("codeg"),
-//         ngx_string("jane") };
-// 
-// static char* descs[] = { "zieckey's id is 1", "codeg's id is 2", "jane's id is 3" };
-// 
-// // hash table的一些基本操作
-// TEST_UNIT(ngx_hash)
-// {
-//     ngx_uint_t          k; //, p, h;
-//     ngx_pool_t*         pool = g_pool;
-//     ngx_hash_init_t     hash_init;
-//     ngx_hash_t*         hash;
-//     ngx_array_t*        elements;
-//     ngx_hash_key_t*     arr_node;
-//     char*               find;
-//     int                 i;
-// 
-//     //ngx_cacheline_size = 32;
-// 
-//     hash = (ngx_hash_t*)ngx_pcalloc(pool, sizeof(ngx_hash_t));
-//     hash_init.hash = hash;                      // hash结构
-//     hash_init.key = &ngx_hash_key_lc;          // hash算法函数
-//     hash_init.max_size = 1024 * 10;                   // max_size
-//     hash_init.bucket_size = 64; // ngx_align(64, ngx_cacheline_size);
-//     hash_init.name = "yahoo_guy_hash";          // 在log里会用到
-//     hash_init.pool = pool;                 // 内存池
-//     hash_init.temp_pool = NULL;
-// 
-//     // 创建数组
-//     elements = ngx_array_create(pool, 32, sizeof(ngx_hash_key_t));
-//     for (i = 0; i < 3; i++) {
-//         arr_node = (ngx_hash_key_t*)ngx_array_push(elements);
-//         arr_node->key = (names[i]);
-//         arr_node->key_hash = ngx_hash_key_lc(arr_node->key.data, arr_node->key.len);
-//         arr_node->value = (void*)descs[i];
-//         // 
-//         printf("key: %s , key_hash: %u\n", arr_node->key.data, arr_node->key_hash);
-//     }
-// 
-//     if (ngx_hash_init(&hash_init, (ngx_hash_key_t*)elements->elts, elements->nelts) != NGX_OK){
-//         return ;
-//     }
-// 
-//     // 查找
-//     k = ngx_hash_key_lc(names[0].data, names[0].len);
-//     printf("%s key is %d\n", names[0].data, k);
-//     find = (char*)
-//         ngx_hash_find(hash, k, (u_char*)names[0].data, names[0].len);
-// 
-//     if (find) {
-//         printf("get desc of %s : %s\n", (char*)names[0].data, (char*)find);
-//     }
-// 
-//     ngx_array_destroy(elements);
-// 
-// }
-// 
-// 
-// 
+
+static ngx_str_t names[] = {
+    ngx_string("zieckey"),
+    ngx_string("codeg"),
+    ngx_string("jane") };
+
+static char* descs[] = { "zieckey's id is 0", "codeg's id is 1", "jane's id is 2" };
+
+// hash table的一些基本操作
+TEST_UNIT(ngx_hash)
+{
+    ngx_uint_t          k;
+    ngx_pool_t*         pool = g_pool;
+    ngx_hash_init_t     hash_init;
+    ngx_array_t*        elements;
+    ngx_hash_key_t*     arr_node;
+    char*               find;
+    int                 i;
+
+    ngx_cacheline_size = NGX_CPU_CACHE_LINE;
+
+    hash_init.hash = NULL;                      // 置为NULL，让ngx_hash_init来初始化
+    hash_init.key = &ngx_hash_key_lc;          // hash算法函数
+    hash_init.max_size = 1024;                   // max_size
+    hash_init.bucket_size = 64; // ngx_align(64, ngx_cacheline_size);
+    hash_init.name = "codeg_hash";          // 在log里会用到
+    hash_init.pool = pool;                 // 内存池
+    hash_init.temp_pool = NULL;
+
+    // 创建数组
+    elements = ngx_array_create(pool, H_ARRAYSIZE(names), sizeof(ngx_hash_key_t));
+    for (i = 0; i < H_ARRAYSIZE(names); i++) {
+        arr_node = (ngx_hash_key_t*)ngx_array_push(elements);
+        arr_node->key = (names[i]);
+        arr_node->key_hash = ngx_hash_key_lc(arr_node->key.data, arr_node->key.len);
+        arr_node->value = (void*)descs[i];
+        printf("key: %s , key_hash: %u\n", arr_node->key.data, arr_node->key_hash);
+    }
+
+    H_TEST_ASSERT(ngx_hash_init(&hash_init, (ngx_hash_key_t*)elements->elts, elements->nelts) == NGX_OK);
+
+    // 查找
+    k = ngx_hash_key_lc(names[0].data, names[0].len);
+    printf("%s key is %u\n", names[0].data, k);
+    find = (char*)ngx_hash_find(hash_init.hash, k, (u_char*)names[0].data, names[0].len);
+    H_TEST_ASSERT(find);
+    if (find) {
+        printf("get desc of %s : %s\n", (char*)names[0].data, (char*)find);
+    }
+
+    ngx_array_destroy(elements);
+}
+
+
+
 
 
 
 // test code is copied from http://blog.csdn.net/livelylittlefish/article/details/6636229
 
 #define Max_Size 1024
-#define Bucket_Size 64  //256, 64
+#define Bucket_Size 32  //256, 64, 32
 
 #define NGX_HASH_ELT_SIZE(name)               \
     (sizeof(void *)+ngx_align((name)->key.len + 2, sizeof(void *)))
@@ -168,7 +161,7 @@ TEST_UNIT(ngx_hash_by_abo)
 
     find_test(hash, urls2, Max_Num2, false);
 
-ExitHandler : 
+ExitHandler:
     //release
     ngx_array_destroy(array);
     ngx_destroy_pool(pool);
@@ -188,7 +181,8 @@ ngx_hash_t* init_hash(ngx_pool_t *pool, ngx_array_t *array)
     */
     ngx_hash_init_t hinit;
 
-    ngx_cacheline_size = 32;  //here this variable for nginx must be assigned a value.
+    ngx_cacheline_size = NGX_CPU_CACHE_LINE;
+
     hinit.hash = NULL;  //if hinit.hash is NULL, it will alloc memory for it in ngx_hash_init
     hinit.key = &ngx_hash_key_lc;  //hash function
     hinit.max_size = Max_Size;
@@ -237,7 +231,7 @@ void dump_hash(ngx_hash_t *hash, ngx_array_t *array)
 {
     int loop;
     char prefix[] = "          ";
-    u_short test[Max_Num] = { 0 };
+    u_short test[512] = { 0 };
     ngx_uint_t key;
     ngx_hash_key_t* elts;
     int nelts;
@@ -254,6 +248,7 @@ void dump_hash(ngx_hash_t *hash, ngx_array_t *array)
     }
     printf("\n");
 
+    // 完全按照hash表的内存结构来遍历
     elts = (ngx_hash_key_t*)array->elts;
     nelts = array->nelts;
     for (loop = 0; loop < nelts; loop++)
@@ -305,16 +300,16 @@ void find_test(ngx_hash_t *hash, ngx_str_t addr[], int num, bool expect_found)
         key = ngx_hash_key_lc(addr[loop].data, addr[loop].len);
         char *value = (char*)ngx_hash_find(hash, key, addr[loop].data, addr[loop].len);
         if (expect_found) {
-            H_TEST_ASSERT(value);
+            H_TEST_ASSERT(value != NULL);
             H_TEST_ASSERT(strncmp(value, values[loop], strlen(value)) == 0);
         }
         else {
             H_TEST_ASSERT(!value);
         }
-    
+
         if (value)
         {
-            printf("(url = \"%s\"%.*s, key = %-10lu) found, (ip = \"%s\")\n",
+            printf("(url = \"%s\"%.*s, key = %-10u) found, (ip = \"%s\")\n",
                 addr[loop].data, Max_Url_Len - addr[loop].len, prefix, key, (char*)value);
         }
         else
