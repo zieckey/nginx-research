93fbb28d634ca830ca2f1eea95e4c149de34e517
weizili.build17 <zieckey@gmail.com>
2015-01-04 10:58:41 +0800

    fix warning

--- libnginx/dump_nginx_struct.h
+++ libnginx/dump_nginx_struct.h
@@ -7,19 +7,19 @@ void dump_pool(ngx_pool_t* pool)
 {
     while (pool)
     {
-        printf("pool = 0x%x\n", pool);
+        printf("pool = 0x%p\n", pool);
         printf("  .d\n");
-        printf("    .last = 0x%x\n", pool->d.last);
-        printf("    .end = 0x%x\n", pool->d.end);
-        printf("    .next = 0x%x\n", pool->d.next);
-        printf("    .failed = %d\n", pool->d.failed);
-        printf("  .max = %d\n", pool->max);
-        printf("  .current = 0x%x\n", pool->current);
-        printf("  .chain = 0x%x\n", pool->chain);
-        printf("  .large = 0x%x\n", pool->large);
-        printf("  .cleanup = 0x%x\n", pool->cleanup);
-        printf("  .log = 0x%x\n", pool->log);
-        printf("available pool memory = %d\n\n", pool->d.end - pool->d.last);
+        printf("    .last = 0x%p\n", pool->d.last);
+        printf("    .end = 0x%p\n", pool->d.end);
+        printf("    .next = 0x%p\n", pool->d.next);
+        printf("    .failed = %u\n", (unsigned int)pool->d.failed);
+        printf("  .max = %lu\n", pool->max);
+        printf("  .current = 0x%p\n", pool->current);
+        printf("  .chain = 0x%p\n", pool->chain);
+        printf("  .large = 0x%p\n", pool->large);
+        printf("  .cleanup = 0x%p\n", pool->cleanup);
+        printf("  .log = 0x%p\n", pool->log);
+        printf("available pool memory = %ld\n\n", pool->d.end - pool->d.last);
         pool = pool->d.next;
     }
 }
